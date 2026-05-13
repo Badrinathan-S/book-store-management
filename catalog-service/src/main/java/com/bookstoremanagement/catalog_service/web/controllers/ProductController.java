6fba8cb696763023d2b3042f867a00206630c6e2
Badrinathan-S <badrinathan3@gmail.com>
2026-05-13 02:33:36 +0000

    core implementation completed

--- catalog-service/src/main/java/com/bookstoremanagement/catalog_service/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/bookstoremanagement/catalog_service/web/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package com.bookstoremanagement.catalog_service.web.controllers;
 
 
+import com.bookstoremanagement.catalog_service.domain.PagedResult;
+import com.bookstoremanagement.catalog_service.domain.Product;
+import com.bookstoremanagement.catalog_service.domain.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +13,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-public class ProductController {
+class ProductController {
+    private final ProductService productService;
+
+    ProductController(ProductService productService){
+        this.productService = productService;
+    }
 
     @GetMapping
-    List<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
-        return productService.getProducts();
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
     }
 }
