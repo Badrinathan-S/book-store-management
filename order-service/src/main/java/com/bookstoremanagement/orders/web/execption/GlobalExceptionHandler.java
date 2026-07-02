36f515b6ea08ac04789c27c247de2f98d8e6e334
Badrinathan-S <badrinathan3@gmail.com>
2026-07-02 04:12:46 +0000

    order service with rabbitMQ

--- order-service/src/main/java/com/bookstoremanagement/orders/web/execption/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/bookstoremanagement/orders/web/execption/GlobalExceptionHandler.java
@@ -1,11 +1,31 @@
 package com.bookstoremanagement.orders.web.execption;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.time.Instant;
 
 @RestControllerAdvice
 class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    private static final URI NOT_FOUND_TYPE = URI.create("");
+    private static final URI NOT_FOUND_TYPE = URI.create("https://api.bookstore.com/error/not-found");
+    private static final URI ISE_FOUND_TYPE = URI.create("https://api.bookstore.com/error/server-error");
+    private static final URI SERVICE_NAME = URI.create("order-service");
+
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail handleUnhandledException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setType(ISE_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
 }
--- order-service/src/test/java/com/bookstoremanagement/orders/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/bookstoremanagement/orders/OrderServiceApplicationTests.java
@@ -1,11 +1,13 @@
 package com.bookstoremanagement.orders;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ConfigurationPropertiesScan
 class OrderServiceApplicationTests {
 
     @Test
